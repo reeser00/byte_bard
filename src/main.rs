49a59b0f31f0afec7f94372ac256a7435c26c53c
reeser00 <neumann.marvin00@gmail.com>
2024-02-22 16:27:21 +0100

    stable 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use proxy::Proxy;
-fn main() {
+use tokio;
+#[tokio::main]
+async fn main() {
     let home_addr = String::from("127.0.0.1:15201");
     let server_addr = String::from("85.17.202.49:15201");
-    let mut my_proxy = Proxy::from(home_addr, server_addr).unwrap();
-    my_proxy.start();
+    let mut my_proxy = Proxy::from(home_addr, server_addr).await.unwrap();
+    my_proxy.start().await;
+    println!("{:#?}", my_proxy);
     loop{}
 }
